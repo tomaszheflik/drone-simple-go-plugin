6e6518b7603db89b4b188cb28eb195f261aee4dd
Tomasz Heflik <heflik@gmail.com>
2017-10-20 15:41:40 +0200

    Add defaults if env doesn't exist

--- main.go
+++ main.go
@@ -15,17 +15,21 @@ type Weter struct {
 }
 
 func main() {
-	weter := Weter{
-		Url: "http://samples.openweathermap.org",
-		Api: "/data/2.5/weather?q=",
-		Location: "Katowice",
-	}
+	weter := Weter{}
 	// Get weter service url
 	fmt.Println("Get variables from os.envs")
-	//weter.Url = os.Getenv("PLUGIN_URL")
-	//weter.Api = os.Getenv("PLUGIN_API")
-	//weter.Location = os.Getenv("PLUGIN_LOCATION")
-
+	weter.Url = os.Getenv("PLUGIN_URL")
+	if weter.Url == "" {
+		weter.Url = "http://samples.openweathermap.org"
+	}
+	weter.Api = os.Getenv("PLUGIN_API")
+	if weter.Api == "" {
+		weter.Api = "/data/2.5/weather?q="
+	}
+	weter.Location = os.Getenv("PLUGIN_LOCATION")
+	if weter.Location == "" {
+		weter.Location = "Katowice"
+	}
 	fmt.Printf("We got \nURL: %s \nAPI: %s \nLOCATION: %s\n", weter.Url, weter.Api, weter.Location)
 	request := get_data(weter)
 	fmt.Printf("%+v", request)
@@ -33,20 +37,19 @@ func main() {
 
 func get_data(weter Weter) string {
 	body := strings.NewReader("Plugin test\n")
-	url := fmt.Sprintf("http://%s%s?q=%s,uk&appid=b1b15e88fa797225412429c1c50c122a1", weter.Url, weter.Api, weter.Location)
+	url := fmt.Sprintf("%s%s?q=%s,uk&appid=b1b15e88fa797225412429c1c50c122a1", weter.Url, weter.Api, weter.Location)
 	req, err := http.NewRequest("GET", url, body)
 	if err != nil {
-		fmt.Printf("NewRequest Error\n")
+		fmt.Printf("NewRequest Error %v \n", err)
 		os.Exit(1)
 	}
 	c := &http.Client{}
 	response, err := c.Do(req)
 	if err != nil {
-		fmt.Printf("Do Error\n")
+		fmt.Printf("Do Error %v \n", err)
 		os.Exit(1)
 	}
 	defer response.Body.Close()
 	data, err := ioutil.ReadAll(response.Body)
-
 	return string(data)
 }
